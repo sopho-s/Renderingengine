17e8de3ef01098337c3780ab68df82ce6277db67
sopho-s <woodsnicholas01@gmail.com>
2024-05-10 14:15:38 +0100

    quick clean up

--- Renderingengine/Maths/GMaths.cuh
+++ Renderingengine/Maths/GMaths.cuh
@@ -1,7 +1,6 @@
 #ifndef GMATHS_CUH
 #define GMATHS_CUH
 namespace GMaths {
-    __device__ void Add(float val1, float val2, float &out);
     __global__ void VecAdd(float* vec1, float* vec2, float* out);
 	__global__ void MatMulVec4(float* matrix[16], float* vector[4], float* out[4]);
 }
